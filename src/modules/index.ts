2a0b5478f5519537f100a7a8f4e017350701f970
Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2024-07-09 08:38:06 +0000

    Setup workspace

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,33 +1,6 @@
-import { Pizza, PizzaPros } from "./models/Pizza";
-
-const rootElement = document.querySelector('.root')!;
+import "../assets/scss/main.scss";
 
 
-const createPizzaTemplate = (pizza: PizzaPros): string => {
-	return `
-        <div class="pizza">
-            <h2>${pizza.title}</h2>
-            <p class="toppings">${pizza.toppings.join(',')}</p>
-            <p>${pizza.description}</p>
-            <span>£${pizza.price}</span>
-        </div>
-    `;
-}
+const test: string = "testing connection";
 
-const renderTemplete = (templates: string[], parent: Element): void => {
-	const templateElement = document.createElement('template');
-
-	for (const t of templates) {
-		templateElement.innerHTML += t;
-	}
-
-	parent.append(templateElement.content);
-}
-
-document.addEventListener('DOMContentLoaded', async () => {
-	const pizzas = await Pizza.loadAll();
-
-	const pizzaArrayString: string[] = pizzas.map(createPizzaTemplate);
-
-	renderTemplete(pizzaArrayString, rootElement);
-});
+console.log(test);
